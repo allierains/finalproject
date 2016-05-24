0ba41b31ff9599d22c8e44491046cba4a89ef27f
Allie Liu <allieliu@outlook.com>
2016-05-24 10:44:41 -0700

    commiting before i break this entire thing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var app          = express();
 var routes       = require('./routes.js');
 var passport     = require('passport');
 var Strategy     = require('passport-twitter').Strategy;
+var api          = express.Router();
 
 
 
@@ -69,6 +70,7 @@ app.get('/login',
   });
 
 
+
 passport.use(new Strategy({
     consumerKey: process.env.CONSUMER_KEY,
     consumerSecret: process.env.CONSUMER_SECRET,
@@ -90,49 +92,6 @@ passport.use(new Strategy({
   passport.deserializeUser(function(obj, cb) {
     cb(null, obj);
   });
-
-// BEGIN TWITTER API TESTS
-
-api.get('/twitter/:id', function(req, res){
-  console.log("Triggered Twitter API route")
-  oauth2.getOAuthAccessToken('', {
-    'grant_type': 'client_credentials'
-  }, function(err, access_token){
-    if(err) throw err
-    console.log(access_token); //string that we can use to authenticate request
-    var options = {
-      hostname: 'api.twitter.com',
-      path: '/1.1/statuses/user_timeline.json?screen_name=' + req.params.id + '&count=200',
-      headers: {
-        Authorization: 'Bearer ' + access_token
-      }
-    };
-    https.get(options, function(result){
-      var buffer = '';
-      result.setEncoding('utf8');
-      result.on('data', function(data){
-        buffer += data;
-      });
-      result.on('end', function(){
-        var tweets = JSON.parse(buffer);
-        console.log(tweets);
-        res.json(tweets)
-      });
-    });
-  });
-
-})
-
-
-
-
-
-// END TWITTER API TESTS
-
-
-
-
-
 
 
 
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,5 @@
 var User = require('./models/user.js');
 
-Twitter.prototype.getUserTimeline = function(params, callback) {
-  var url = '/statuses/user_timeline.json';
-  this.get(url, params, callback);
-  return this;
-}
 
 
 module.exports = controller;
