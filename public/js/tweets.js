cbba7474b7a1e7f0b63309fff63e8cea21b4e08d
Allie Liu <allieliu@outlook.com>
2016-05-25 11:54:18 -0700

    got tweets to render!

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -1,6 +1,5 @@
 
-
-
+var $fullTweets = $('#display-full-tweets')
 
 $("#button").click(function() {
   console.log('twitterHandle' + $('#twitterHandle').text())
@@ -9,13 +8,33 @@ $("#button").click(function() {
         url: '/api/twitter/' + $('#twitterHandle').text()
       })
       .done(function(data){
-        console.log(data)
+        console.log(data[0].text)
         var arr = [];
+        var tweetsContainer = $('#tweetsContainer');
         for (i=0; i<data.length; i++){
           arr.push(data[i].text)
+          tweetsContainer.append('<li>' + data[i].text + '</li>');
         }
       });
 });
+
+
+
+
+// $("#button").click(function() {
+//   console.log('twitterHandle' + $('#twitterHandle').text())
+//    $.ajax({
+//         method: 'get',
+//         url: '/api/twitter/' + $('#twitterHandle').text()
+//       })
+//       .done(function(data){
+//         console.log(data)
+//         var arr = [];
+//         for (i=0; i<data.length; i++){
+//           arr.push(data[i].text)
+//         }
+//       });
+// });
 
 // $("#button2").click(function() {
 //   console.log('hi');
