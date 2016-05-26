63a18899941c93ca7b29eaeb82e576d1b2e0913a
Allie Liu <allieliu@outlook.com>
2016-05-26 11:56:41 -0700

    in progress

--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ module.exports = function(app, passport) {
         res.redirect('/');
     });
 
+
     // route for twitter authentication and login
 
     // handle the callback after twitter has authenticated the user
